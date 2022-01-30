8f3debf1777d4d862500b0493d4d4639cbae1efe
John Preston <git-9282@jpreston.xyz>
2022-01-30 18:13:14 +0000

    wip

--- README.md
+++ README.md
@@ -4,10 +4,12 @@ This is my (John L. Preston's) entry for the [MoneyLab NFT Essay prize](https://
 
 The question to be addressed is: **"Are NFTs the key to a generation's empowerment, or a capitalist trap?"**
 
-This essay has been developed using Git as a narrative medium. This repository contains two text files: `nft-good.md` argues that "NFTs the key to a generation's empowerment" whereas `nft-bad.md` argues that "NFTs are a capitalist trap". Additionally the Git commit messages provide tangential discussion on these issues along with an artist's commentary on the work itself and the development process.
+This essay has been developed using Git as a narrative medium. This repository contains two text files: `nft-good.md` argues that "NFTs the key to a generation's empowerment" whereas `nft-bad.md` argues that "NFTs are a capitalist trap". Additionally the Git commit messages provide tangential discussion on these issues along with an artist's commentary on the work itself.
 
 To experience this essay in its intended medium, you should:
 
 1. Clone this Git repository -- `git clone git@github.com:wcerfgba/moneylab-nft-essay.git`
 2. Checkout the `mainnet` branch -- `git checkout mainnet`
-3. Display the log in reverse order with raw patch and raw commit log -- `git log --patch-with-raw --format=raw`
+3. Display the log in reverse order with raw patch and raw commit log -- `git log --reverse --patch-with-raw --format=raw`
+
+This will show you the full Git 'blockchain', and display all three mini-essays in a line-by-line format. You may also prefer to read each essay separately, by viewing `nft-good.md` and `nft-bad.md` with your preferred text editors, and by viewing the commit messages with `git log --reverse --format='%s'`
--- commitalise.js
+++ commitalise.js
@@ -3,31 +3,33 @@ const Git = require('nodegit');
 
 const root = '../moneylab-nft-essay-2';
 
-const repo = await Git.Repository.open(root);
-await repo.checkoutBranch('mainnet');
+(async function () {
+  const repo = await Git.Repository.open(root);
+  //await repo.checkoutBranch('mainnet');
 
-let commits = await fs.promises.readFile('commits.md');
-let nft_good = await fs.promises.readFile('nft-good.md');
-let nft_bad = await fs.promises.readFile('commits.md');
+  let commits = await fs.promises.readFile('commits.md');
+  let nft_good = await fs.promises.readFile('nft-good.md');
+  let nft_bad = await fs.promises.readFile('nft-bad.md');
 
-commits = commits.split('\n');
-nft_good = nft_good.split('\n');
-nft_bad = nft_bad.split('\n');
+  commits = commits.toString().split('\n');
+  nft_good = nft_good.toString().split('\n');
+  nft_bad = nft_bad.toString().split('\n');
 
-for (let i = 0; i < commits.length - 1; i++) {
-  await fs.promises.appendFile(root + '/nft-good.md', nft_good[i] + '\n');
-  await fs.promises.appendFile(root + '/nft-bad.md', nft_bad[i] + '\n');
+  for (let i = 1; i < commits.length - 1; i++) {
+    await fs.promises.appendFile(root + '/nft-good.md', nft_good[i] + '\n');
+    await fs.promises.appendFile(root + '/nft-bad.md', nft_bad[i] + '\n');
 
-  const index = await repo.refreshIndex();
+    const index = await repo.refreshIndex();
 
-  await index.addByPath(root + '/nft-good.md');
-  await index.addByPath(root + '/nft-bad.md');
-  await index.write();
+    await index.addByPath('nft-good.md');
+    await index.addByPath('nft-bad.md');
+    await index.write();
 
-  const oid = await index.writeTree();
+    const oid = await index.writeTree();
 
-  const parent = await repo.getHeadCommit();
-  const author = nodegit.Signature.now('John L. Preston', 'git-9282@jpreston.xyz');
+    const parent = await repo.getHeadCommit();
+    const author = Git.Signature.now('John L. Preston', 'git-9282@jpreston.xyz');
 
-  const commitId = await repo.createCommit("HEAD", author, author, commits[i], oid, [parent]);
-}
+    const commitId = await repo.createCommit("HEAD", author, author, commits[i], oid, [parent]);
+  }
+})()
